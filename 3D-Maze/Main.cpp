fdb269c691f8113047dd22522d30f05bc50675f9
Jian-Bin Liu <billyljb@gmail.com>
2017-05-02 13:16:08 +0800

    update

--- 3D-Maze/Main.cpp
+++ 3D-Maze/Main.cpp
@@ -1,16 +1,34 @@
 #include "Angel.h"
+#include <iostream>
+#include <stack>
+using namespace std;
+
 typedef Angel::vec4 color4;
 typedef Angel::vec4 point4;
+const int numVertices = 1600;
+const int numOfCubePoints = 8;
 const int WIDTH = 500;
 const int HEIGHT = 500;
 const int colCount = 10;
 const int rowCount = 10;
-int maze_area = colCount * rowCount;
-const int numVertices = 1600;
-const int numOfCubePoints = 8;
+int mazeCount = colCount * rowCount;
+int *mazeArray = (int *)malloc(mazeCount * sizeof(int *));
 
 point4 points[numVertices];
 color4 colors[numVertices];
+
+class Cell {
+	int numCell;
+	bool wallTop = true;
+	bool wallRight = true;
+	bool wallBottom = true;
+	bool wallLeft = true;
+	bool visited = false;
+};
+
+vector<Cell> grid;
+Cell currentCell;
+stack<Cell> cellPath;	// for recording the modification of current cell into stack
 
 
 void init() {
@@ -19,11 +37,16 @@ void init() {
 }
 
 void display() {
-
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
 
 void keyboard(unsigned char key, int x, int y) {
-
+	switch (key) {
+	case 033: // Escape Key
+	case 'q': case 'Q':
+		exit(EXIT_SUCCESS);
+		break;
+	}
 	glutPostRedisplay();
 }
 
